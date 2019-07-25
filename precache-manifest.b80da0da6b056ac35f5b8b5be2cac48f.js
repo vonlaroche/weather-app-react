3abb11586c8298f2b7ca66e7f6ef1e968126e380
Monika <monika.dominic@recordunion.com>
2019-07-25 10:04:11 +0200

    Updates

---
+++ precache-manifest.b80da0da6b056ac35f5b8b5be2cac48f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30af6f81cd6129e42b731175ce9e3846",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "fea3070eae813dc0a4cd",
+    "url": "/weather-app-react/static/css/main.3f298689.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/weather-app-react/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "fea3070eae813dc0a4cd",
+    "url": "/weather-app-react/static/js/main.801df859.chunk.js"
+  },
+  {
+    "revision": "b246a4fc453f9ee8e4e8",
+    "url": "/weather-app-react/static/js/runtime~main.a31b442a.js"
+  },
+  {
+    "revision": "9d2b3ea185ffe7e080db76ff667fd702",
+    "url": "/weather-app-react/static/media/cloudy.9d2b3ea1.png"
+  },
+  {
+    "revision": "5ebf4695df8af47d8298243a13f28564",
+    "url": "/weather-app-react/static/media/rain.5ebf4695.png"
+  },
+  {
+    "revision": "99a9fb6255bceabddb89f79bbf9a695c",
+    "url": "/weather-app-react/static/media/sun.99a9fb62.png"
+  }
+]);
